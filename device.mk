crdroidandroid/android_device_xiaomi_daisy

daisy: rootdir: Change how we set the alarm property
* We used to have an init extension, which breaks treble
  and since P it requires custom SELinux rules to allow
  init reading /proc/sys/kernel/boot_reason
* Remove the init extension and set the property like
  QCOM does through init.qcom.early_boot.sh
* Update the property with vendor prefix while we are at it

Change-Id: I1d2a42bc078133e9830a48c84bc0fb104f889189
Signed-off-by: LinkBoi00 <50962670+LinkBoi00@users.noreply.github.com>

device.mk
@@ -351,6 +351,7 @@ PRODUCT_PACKAGES += \
 
 PRODUCT_PACKAGES += \
     init.qcom.bt.sh \
+    init.qcom.early_boot.sh \
     init.qcom.post_boot.sh
 
 PRODUCT_PACKAGES += \

100Daisy/12321

device.mk
@@ -338,7 +338,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     init.qcom.sh \
     init.qcom.bt.sh \
-    init.qcom.post_boot.sh
+    init.qcom.post_boot.sh \
+    init.qcom.early_boot.sh
 
 # RenderScript
 PRODUCT_PACKAGES += \